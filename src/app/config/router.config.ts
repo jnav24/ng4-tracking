250f8385e58e736b651b5047035594462a700359
Justin Navarro <info@justinnavarro.net>
2017-12-04 17:11:12 -0500

    add invoice to route

--- src/app/config/router.config.ts
+++ src/app/config/router.config.ts
@@ -3,17 +3,19 @@ import {SignInComponent} from '../sign-in/sign-in.component';
 import {DashboardComponent} from '../dashboard/dashboard.component';
 import {DashboardProjectsComponent} from "../dashboard/dashboard-projects/dashboard-projects.component";
 import {DashboardTrackerComponent} from "../dashboard/dashboard-tracker/dashboard-tracker.component";
+import {DashboardInvoiceComponent} from '../dashboard/dashboard-invoice/dashboard-invoice.component';
 
 export const RouterConfig: Route[] = [
 	{ path: '', component: SignInComponent },
 	{ path: 'login', component: SignInComponent },
 	{ path: 'register', component: SignInComponent },
-	{ 
+	{
 		path: 'dashboard',
 		children: [
 			{ path: ':uid', component: DashboardComponent },
 			{ path: ':uid/:cid', component: DashboardProjectsComponent },
-			{ path: ':uid/:cid/:pid', component: DashboardTrackerComponent }
+			{ path: ':uid/:cid/:pid', component: DashboardTrackerComponent },
+      { path: 'invoice/:uid', component: DashboardInvoiceComponent }
 		]
 	},
 	{ path: '**', redirectTo: 'login' },
